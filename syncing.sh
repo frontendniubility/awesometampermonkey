e500b152eb6c2b190b8816e57a6c4010476073d8
68681395 <happycoder@foxmail.com>
2020-11-18 10:26:18 +0800

    0

--- syncing.sh
+++ syncing.sh
@@ -1,17 +1,18 @@
-
-read -r -p "Wait 5 seconds or press any key to continue immediately" -t 5 -n 1 -s
-
-
+#!/bin/bash
+ 
 echo -n "输入提交介绍，默认为‘Just auto-commit for synchronizing workspace’ ->"
-read msg 
- if [ -z "$msg" ]; then
-     echo "committing as comment 'auto-commit for synchronizing'"
-	 msg="auto-commit for synchronizing"
- fi
+
+read -t 5 -n 50
+if [ $? = 0 ] ; then
+	$?="auto-commit for synchronizing"
+else
+echo "waiting for the keypress"
+fi
+ 
 git pull
 git add *
 git add -A
-git commit -m "$msg"
+git commit -m "$?"
 git push -u origin master
 
 pause
