6c169cee78bbb1fb9f6f83578cb41e4cf59f8332
68681395 <happycoder@foxmail.com>
2020-11-18 10:14:37 +0800

    auto-commit for synchronizing

--- syncing.sh
+++ syncing.sh
@@ -1,5 +1,9 @@
+
+read -r -p "Wait 5 seconds or press any key to continue immediately" -t 5 -n 1 -s
+
+
 echo -n "输入提交介绍，默认为‘Just auto-commit for synchronizing workspace’ ->"
-read msg  -p "Pause Time 5 seconds" -t 5
+read msg 
  if [ -z "$msg" ]; then
      echo "committing as comment 'auto-commit for synchronizing'"
 	 msg="auto-commit for synchronizing"
