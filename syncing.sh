788cbd7e442006090c9a92c65e4bd79da27237fc
68681395 <happycoder@foxmail.com>
2020-11-18 10:51:13 +0800

    0

--- syncing.sh
+++ syncing.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
  
- if read -t 5 -p "输入网站名:" website
-then
-    echo "你输入的网站名是 $website"
-else
-    echo "\n抱歉，你输入超时了。"
-fi 
  
-sleep 20
- 
-echo  "key in commiting message，default's ‘Just auto-commit for synchronizing workspace’"
 
-read -t 5 -n 50
-if [ $? = 0 ] ; then
-	$?="auto-commit for synchronizing"
+read -p 'commiting message >' -t 5 -n 50
+if [ $ = 0 ] ; then
+	$="auto-commit for synchronizing"
 else
-echo "waiting for the keypress"
+echo "$"
 fi
  
 git pull
@@ -23,5 +14,6 @@ git add *
 git add -A
 git commit -m "$?"
 git push -u origin master
-
-pause
+ 
+ 
+ read 
